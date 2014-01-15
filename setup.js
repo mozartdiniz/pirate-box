edda01a8c9e321b45fba1224a368f1ec1c9ced4e
Fernando Nogueira <fernandojnn@gmail.com>
2014-01-15 13:01:58 -0300

    Initial Database structure created.

--- setup.js
+++ setup.js
@@ -1,20 +1,31 @@
+console.log("Starting setup...");
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('database.sqlite');
 
+console.log("Creating database...");
+
 db.serialize(function() {
-  db.run("CREATE TABLE IF NOT EXISTS subscription (id INTEGER PRIMARY KEY, description TEXT);");
 
-  db.run("CREATE TABLE IF NOT EXISTS subscriptionterms (id INTEGER PRIMARY KEY,	subscriptionid INTEGER,	FOREIGN KEY(subscriptionid) REFERENCES subscription(id))");
+  console.log("Creating configuration table...");
+  db.run("CREATE TABLE IF NOT EXISTS configuration (id INTEGER PRIMARY KEY, defaultepisodetemplate TEXT)");
 
-  // var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-  // for (var i = 0; i < 10; i++) {
-  //     stmt.run("Ipsum " + i);
-  // }
-  // stmt.finalize();
+  console.log("Creating subscription table...");
+  db.run("CREATE TABLE IF NOT EXISTS subscription (id INTEGER PRIMARY KEY, description TEXT, episodenumbertemplate TEXT, latestcheck TEXT)");
 
-  // db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
-  //     console.log(row.id + ": " + row.info);
-  // });
+  console.log("Creating subscriptionoption table...");
+  //id, subscriptionid, value, type (e.g. 1, 1, arrow, TERM) (types: TERM, CONTAINS, NOT_CONTAINS)
+  db.run("CREATE TABLE IF NOT EXISTS subscriptionoption (id INTEGER PRIMARY KEY,	subscriptionid INTEGER,	value TEXT, type TEXT, FOREIGN KEY(subscriptionid) REFERENCES subscription(id))");
+
+
+  console.log("Creating downloadedepisode table...");
+  db.run("CREATE TABLE IF NOT EXISTS downloadedepisode (id INTEGER PRIMARY KEY, subscriptionid INTEGER, name TEXT, season TEXT, episode TEXT, date TEXT)");
+
+  console.log("Inserting default values...");
+  var stmt = db.prepare("INSERT INTO configuration VALUES(?, ?)");
+  stmt.run(1,'S%SEASON%E%EPISODE%');
+  stmt.finalize();
 });
 
 db.close();
+
+console.log("Setup finished.");
