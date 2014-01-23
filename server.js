e601674c365dbe80c1e7c3f08ce720e99abba2b3
Mozart Diniz <mozartdiniz@mozarts-mbp.home>
2014-01-22 22:01:22 -0300

    Adicionei o twitter bootstrap, criei uma interface mínima, agora pode subir o servidor node server.js, a acessar http://localhost:3000

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express'),
+    finder = require("./torrentfinder"),
     http = require('http');
 
 var app = express();
@@ -15,15 +16,46 @@ app.configure(function() {
     app.use(express.static(__dirname + '/public'));
 
     app.use(express.errorHandler({
-        dumpExceptions: true
-        , showStack: true
+        dumpExceptions: true,
+        showStack: true
     }))
 });
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
     res.render('index.html', {
         title: "Index"
     });
+});
+
+io.sockets.on('connection', function (socket) {
+
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+
+  socket.on('search', function (data) {
+
+    var mediaOptions = {};
+
+    var terms = data.terms.split(' ');
+
+    var contains = new Array();
+    contains[0] = "720p";
+
+    mediaOptions.terms = terms;
+    mediaOptions.contains = contains;
+
+    mediaOptions.doNotContain = new Array();
+
+    var callbackFunctionWithBestMatch = function(result) {
+        socket.emit ('searchReturn', result);
+    };
+
+    finder.Search(terms, null, null, callbackFunctionWithBestMatch);    
+
+  })
+
 });
 
 console.log("Express server listening on port 3000");
--- test-files/play.js
+++ test-files/play.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var lame = require('lame');
 var Speaker = require('speaker');
- 
+
 fs.createReadStream(process.argv[2])
 	.pipe(new lame.Decoder())
 	.on('format', function (format) {
